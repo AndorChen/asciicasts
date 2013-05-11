389de94e6ae726a08599ee1e6ce187f08c6c26e5
Andor Chen <Andor.Chen.27@gmail.com>
2013-05-11 13:29:32 +0800

    fix the posts sort

--- _plugins/sortedposts_generator.rb
+++ _plugins/sortedposts_generator.rb
@@ -4,7 +4,7 @@ module Jekyll
     safe true
 
     def generate(site)
-      site.config['sorted_posts'] = site.posts.sort { |x, y| x.data['episode'] <=> y.data['episode'] }
+      site.config['sorted_posts'] = site.posts.sort { |x, y| y.data['episode'] <=> x.data['episode'] }
     end
 
   end
